e3db96c3e4839e5fb957b299154bbef91acb1d2e
janardhangrd <57827513+janardhangrd@users.noreply.github.com>
2021-09-16 07:57:13 +0530

    Section 4
    Hashing Password, JWT TokenBased Authentication, DRY-Donot Repeat Yourself, DTO's, Validation, Using Services in C#, Extension Methods.

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -1,10 +1,15 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -20,15 +25,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddCors();
         }
@@ -49,6 +51,7 @@ namespace API
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
