cfe245966acb0ea3712e142119a5ad0c3bfb7b3a
janardhangrd <57827513+janardhangrd@users.noreply.github.com>
2021-12-30 15:23:04 +0530

    Routing in Angular

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,22 +13,24 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model:any = {};
   
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toast: ToastrService) { }
 
   ngOnInit(): void {}
 
   login()
   {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl("/members");
+      this.toast.success("Login Successfull");
     },
     error => {
-      console.log(error);
+      this.toast.error(error.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 
 }
--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,17 +10,17 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
 model:any = {};
 @Output() cancelRegister = new EventEmitter();
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   register(){
     this.accountService.register(this.model).subscribe( response => {
-      console.log("RegisterComponent:: register: succesfull.");
+      this.toast.success("Registration Successfull.");
       this.cancel();
     }, error => {
-      console.log("RegisterComponent:: register: failed.");
+      this.toast.error("Registration Failed." + error.error);
     });
   }
   cancel(){
