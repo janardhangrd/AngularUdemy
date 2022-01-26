a157b91ddc7266b6cd6d9b80f8c9b006ab8364c5
janardhangrd <57827513+janardhangrd@users.noreply.github.com>
2022-01-26 08:23:19 +0530

    End of Section 10

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,7 +40,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
             };
         }
 
@@ -52,6 +53,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
+                .Include(p=>p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null)
             {
@@ -67,8 +69,9 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };;
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
+            };
         }
     }
 }
--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -8,8 +8,10 @@ using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 namespace API.Controllers
 {
@@ -18,10 +20,13 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController( IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public UsersController( IUserRepository userRepository, IMapper mapper,
+         IPhotoService photoService)
         {
             _mapper = mapper;
             _userRepository = userRepository;
+            _photoService = photoService;
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -37,7 +42,7 @@ namespace API.Controllers
         //     return await _userRepository.GetUserByIdAsync(id);
         // }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             //Without AutoMapper
@@ -50,8 +55,7 @@ namespace API.Controllers
 
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             _mapper.Map(memberUpdateDto,user);
             _userRepository.Update(user);
@@ -59,5 +63,69 @@ namespace API.Controllers
             if(await _userRepository.SaveAllAsync())  return NoContent();
             return BadRequest("Failed to update User.");
         }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file) {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if(result.Error != null) {
+                return BadRequest(result.Error.Message);
+            }
+            var photo = new Photo {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if(user.Photos.Count == 0){
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+            if(await _userRepository.SaveAllAsync()) {
+                //return _mapper.Map<PhotoDto>(photo);
+                return CreatedAtRoute("GetUser",new { username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            }
+
+            return BadRequest("Problem adding Photo.");
+        }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId) {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            var photo = user.Photos.FirstOrDefault(m=>m.Id == photoId);
+
+            if(photo.IsMain) return BadRequest("This is already your main Photo");
+
+            var currentMain = user.Photos.FirstOrDefault(x=>x.IsMain);
+            if(currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to set main photo.");
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId) {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            var photo = user.Photos.FirstOrDefault(m=>m.Id == photoId);
+            if(photo == null) {
+                return BadRequest("photo does not exists");
+            }
+            if(photo.IsMain) return BadRequest("You cannot delete your Main Photo");
+
+            if(photo.PublicId != null) {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if(result.Error != null) {
+                    return BadRequest(result.Error.Message);
+                }
+            }
+            user.Photos.Remove(photo);
+
+            if(await _userRepository.SaveAllAsync()) return Ok();
+            return BadRequest("Failed to delete photo");
+        }
+
     }
 }
